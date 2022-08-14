b1b2cd8f317eb38e922c1615c46f18ef6a6ec183
Alan Jenson <ajenson@itbit.com>
2022-08-14 12:17:00 -0400

    min-cost example

--- src/main/java/challenges/Novator.java
+++ src/main/java/challenges/Novator.java
@@ -1,13 +1,78 @@
 package challenges;
 
+import com.google.ortools.Loader;
+import com.google.ortools.graph.MinCostFlow;
 import problem.Obligation;
+import problem.Party;
 import problem.Problem;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Novator {
 
     public static Collection<Obligation> novateObligations(Problem problem) {
-        return problem.obligations();
+        Loader.loadNativeLibraries();
+
+        var indexToParty = problem.parties().stream().map(Party::name).toArray(String[]::new);
+        var partyToIndex = mapPartyToIndex(indexToParty);
+        var minCostFlow = initializeMinCostFlow(problem, indexToParty, partyToIndex);
+
+        if (minCostFlow.solve() != MinCostFlow.Status.OPTIMAL) {
+            throw new IllegalStateException("optimality not achieved");
+        }
+
+        return createNovatedObligation(indexToParty, minCostFlow);
+    }
+
+    private static LinkedList<Obligation> createNovatedObligation(String[] indexToParty, MinCostFlow minCostFlow) {
+        var novatedObligations = new LinkedList<Obligation>();
+        for (int i = 0; i < minCostFlow.getNumArcs(); ++i) {
+            long cost = minCostFlow.getFlow(i) * minCostFlow.getUnitCost(i);
+            System.out.println(minCostFlow.getTail(i) + " -> " + minCostFlow.getHead(i) + "  "
+                    + minCostFlow.getFlow(i) + "  / " + minCostFlow.getCapacity(i) + "       " + cost);
+            novatedObligations.add(new Obligation(indexToParty[minCostFlow.getTail(i)], indexToParty[minCostFlow.getHead(i)], minCostFlow.getFlow(i)));
+        }
+        return novatedObligations;
+    }
+
+    private static MinCostFlow initializeMinCostFlow(Problem problem, String[] indexToParty, HashMap<String, Integer> partyToIndex) {
+        MinCostFlow minCostFlow = new MinCostFlow();
+        problem.parties()
+                .forEach(party -> party.preferences()
+                        .forEach(preference -> {
+                            minCostFlow.addArcWithCapacityAndUnitCost(
+                                    partyToIndex.get(preference.counterPartyName()),
+                                    partyToIndex.get(party.name()),
+                                    preference.maxExposure(),
+                                    preference.quantifiedDisgust()
+                            );
+                        }));
+
+        long[] supplies = calculateSupplies(problem, indexToParty, partyToIndex);
+        for (int i = 0; i < indexToParty.length; i++) {
+            minCostFlow.setNodeSupply(i, supplies[i]);
+        }
+        return minCostFlow;
+    }
+
+    private static HashMap<String, Integer> mapPartyToIndex(String[] indexToParty) {
+        var partyToIndex = new HashMap<String, Integer>(indexToParty.length);
+        for (int i = 0; i < indexToParty.length; i++) {
+            partyToIndex.put(indexToParty[i], i);
+        }
+        return partyToIndex;
+    }
+
+    private static long[] calculateSupplies(Problem problem, String[] indexToParty, HashMap<String, Integer> partyToIndex) {
+        var supplies = new long[indexToParty.length];
+        problem.obligations().forEach(obligation -> {
+            var fromIndex = partyToIndex.get(obligation.fromParty());
+            var toIndex = partyToIndex.get(obligation.toParty());
+            supplies[fromIndex] = supplies[fromIndex] + obligation.dollars();
+            supplies[toIndex] = supplies[toIndex] - obligation.dollars();
+        });
+        return supplies;
     }
 }
