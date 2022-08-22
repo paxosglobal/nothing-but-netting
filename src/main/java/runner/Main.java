8e928208d03e5f890452d3d0b511e7081a77a1a5
Alan Jenson <ajenson@itbit.com>
2022-08-21 22:44:52 -0400

    cost per dollar

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -11,6 +11,6 @@ class Main {
         Validator.validate(novatedObligations, problem);
         final var efficiencyScore = Scorer.score(novatedObligations, problem);
         novatedObligations.forEach(System.out::println);
-        System.out.printf("\n>>>>> Your efficiency score was %s (golf rules) <<<<<%n", efficiencyScore);
+        System.out.printf("\n>>>>> Your total cost was %s <<<<<", efficiencyScore);
     }
 }
